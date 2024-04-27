4301eeedd6dc599ec163695abdb7f23c8a895910
antimacy <yihhxxyy@outlook.com>
2024-04-27 21:39:53 +0800

    fetch the games

---
+++ src/services/api-client.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+
+//create an axios instance 
+export default axios.create({
+    baseURL: "https://api.rawg.io/api",
+    params: {
+        key: '35b309bd9d7748f99fc22782c8ad0ddb'
+    }
+})
+
+
