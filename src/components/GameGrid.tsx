0dc19658d061d5e08e16defcebdc88286ec0aa2e
antimacy <yihhxxyy@outlook.com>
2024-04-30 23:12:11 +0800

    show loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,12 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  //render 6 skeletons
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   //note: components should be primarily responsible for returning mark-up and handling user interaction at a high level
   return (
@@ -14,6 +17,8 @@ const GameGrid = () => {
         spacing={10}
         padding="10px"
       >
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -27,24 +27,33 @@ const useGames = () => {
 
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
+    
 
   //use the effect hook to send the fetch request to the backend
   useEffect(() => {
     const controller = new AbortController(); //to handle cancellation
 
+    // before calling the api, set loading to true; when done, set to false
+    setLoading(true);
+
     apiClient
       .get<FetchGamesResponse>("/games", {signal: controller.signal})
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results)
+        setLoading(false);
+    })
       .catch((error) => {
         if (error instanceof CanceledError) return;
         setError(error.message)
+        setLoading(false);
       });
 
     
     return () => controller.abort(); //clean-up function
   }, []); //without [] (the dependency), we will constantly send request to backend
 
-  return { games, error };
+  return { games, error, isLoading };
 }
 
 export default useGames;
