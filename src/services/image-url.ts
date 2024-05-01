bb7981550271768f332ba632fb5fd11c927937b7
antimacy <yihhxxyy@outlook.com>
2024-05-01 22:00:54 +0800

    handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,9 @@
+import noImage from "../assets/no-image-placeholder.webp"
+
 
 const getCroppedImageURL = (url: string) => {
     if(!url) {
-        return '';
+        return noImage;
     }
     const target = "media/";
     const index = url.indexOf(target) + target.length;
